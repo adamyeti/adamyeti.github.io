324a3fa8f72b724733ca6068e871b7845ea4a19d
adamyeti <adamracer00@yahoo.com>
2016-09-25 20:02:25 +0000

    added F-ingLoad Respring Logo

--- packages.sh
+++ packages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # dpkg-deb -bZbzip projects/<project name> <ouput folder>
-dpkg-deb -bZgzip projects/CustomRespring debs
+dpkg-deb -bZgzip projects/AndroidRespringLogos debs
+dpkg-deb -bZgzip projects/F-ingLoad debs
