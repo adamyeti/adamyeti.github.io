2866027f9a2a8077bcda608d721a7d2f56c41d4e
adamyeti <adamracer00@yahoo.com>
2016-09-28 21:33:42 +0000

    Added Honey Badger Respring Logo

--- packages.sh
+++ packages.sh
@@ -2,3 +2,4 @@
 # dpkg-deb -bZbzip projects/<project name> <ouput folder>
 dpkg-deb -bZgzip projects/AndroidRespringLogos debs
 dpkg-deb -bZgzip projects/F-ingLoad debs
+dpkg-deb -bZgzip projects/HoneyBadger debs
